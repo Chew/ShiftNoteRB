dc998ddb5fff1ede0bb8f0db64669c214864cdae
Chew <chew@chew.pw>
2019-06-17 15:34:07 -0500

    add more info to employee

--- lib/shiftnote/employee_overview_view_model.rb
+++ lib/shiftnote/employee_overview_view_model.rb
@@ -10,13 +10,38 @@ class ShiftNote::EmployeeOverviewViewModel
     @email = data['email']
     @birthday = Time.parse(data['BirthDate'])
     @schedule_this_week = ShiftNote::ScheduleThisWeekViewModel.new(data['ScheduleThisWeekViewModel'])
+    @trade_shifts = data['TradeShifts']
+    @trade_shifts_current_day = data['TradeShiftsCurrentDay']
+    @positions = data['ThisEmployeePositions']
+    @last_day = Time.parse(data['LastDay'])
+    @hire_day = Time.parse(data['HireDate'])
   end
 
+  # This is the employee's ID. Really has no use outside of Shiftnote internal data
+  # @return [Integer] the employee's internal ID
   attr_reader :id
+
+  # The employee's (legal) name.
+  # @return [String] the employee's name
   attr_reader :name
+
+  # An array containing the user's home and mobile phone numbers
+  # @return [Array<String>] the user's phone numbers.
   attr_reader :phones
+
+  # @return [String] the user's email.
   attr_reader :email
+
+  # @return [Time] the user's birthday
   attr_reader :birthday
+
+  # The user's "Schedule This Week," consider the return type for info
+  # @return [ShiftNote::ScheduleThisWeekViewModel]
   attr_reader :schedule_this_week
   attr_reader :raw
+  attr_reader :trade_shifts
+  attr_reader :trade_shifts_current_day
+  attr_reader :positions
+  attr_reader :last_day
+  attr_reader :hire_day
 end
